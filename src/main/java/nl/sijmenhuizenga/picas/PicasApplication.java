feb205d1f64bcb962cc4bb330569e19e1dda6e74
sijmenhuizenga <sijmenhuizenga@gmail.com>
2020-09-04 13:08:06 +0200

    Generated from spring initializr

---
+++ src/main/java/nl/sijmenhuizenga/picas/PicasApplication.java
@@ -0,0 +1,13 @@
+package nl.sijmenhuizenga.picas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PicasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PicasApplication.class, args);
+	}
+
+}
---
+++ src/test/java/nl/sijmenhuizenga/picas/PicasApplicationTests.java
@@ -0,0 +1,13 @@
+package nl.sijmenhuizenga.picas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PicasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
